d3345feee699a5c4627a230aba06c39fae969048
akshaykarkare <akshay.karkare@gmail.com>
2022-02-03 13:34:04 +0530

    removed .gitignore

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -9,7 +9,7 @@ import java.io.File;
 @CucumberOptions(
         features = "features",
         glue = {"stepdefinition"},
-        tags = "@zoho",
+        tags = "@test",
         plugin = {"html:target/cucumber-reports/htmlReports.html","json:target/cucumber-reports/cucumber.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true
 )
